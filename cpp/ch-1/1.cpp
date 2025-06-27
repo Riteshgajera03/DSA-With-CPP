9da88ab9983818f8312f426ab72c4f1792c6a80e
Riteshgajera03 <riteshgajera55@gmail.com>
2025-06-27 10:28:12 +0530

    hello

---
+++ cpp/ch-1/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name\t : Ritesh Gajera" << endl;
+    cout << "Age\t : 19" << endl;
+    cout << "School\t : Ad-2" << endl;
+
+}
